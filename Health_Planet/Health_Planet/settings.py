5c8643dfc4f121975a0c9fc71a2844ebed91f33a
Achiek <dengachiek54@gmail.com>
2026-05-08 11:16:31 +0300

    Fix Render static asset paths

--- Health_Planet/Health_Planet/settings.py
+++ Health_Planet/Health_Planet/settings.py
@@ -152,9 +152,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE_BACKEND = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 if HAS_WHITENOISE:
